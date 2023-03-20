81f5bc9a766653f423424b3ce2c8fa678c87fba6
danielvargass97 <daniel_vrgs@hotmail.com>
2023-03-20 18:05:00 -0500

    Finish lab

--- app/src/main/java/com/dvargas/adaschool/retrofit/MainActivity.kt
+++ app/src/main/java/com/dvargas/adaschool/retrofit/MainActivity.kt
@@ -2,16 +2,12 @@ package com.dvargas.adaschool.retrofit
 
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.dvargas.adaschool.retrofit.databinding.ActivityMainBinding
+import com.dvargas.adaschool.retrofit.network.DogsImageService
 import com.dvargas.adaschool.retrofit.repository.DogsImageRepository
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -19,65 +15,49 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
-    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        setContentView(R.layout.activity_main)
 
         val dogsImageService = DogsImageRepository.create()
+        getRandomDogByDogBreed(dogsImageService, "boxer")
+        getRandomDog(dogsImageService)
 
-        GlobalScope.launch(Dispatchers.IO) {
-            val response = dogsImageService.getRandomDogImage()
-            if (response.isSuccessful) {
-                val randomDogImageDto = response.body()
-                Log.d("Developer", "Response:  $randomDogImageDto")
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    fun getRandomDogByDogBreed(dogsImageService: DogsImageService, dogBreed: String) {
+        val button = findViewById<Button>(R.id.button1)
+        button.setOnClickListener {
+            GlobalScope.launch(Dispatchers.IO) {
+                val imageView = findViewById<ImageView>(R.id.imageView)
+                val response = dogsImageService.getRandomImageByDogBreed(dogBreed)
+                if (response.isSuccessful) {
+                    val randomDogImageDto = response.body()
+                    Log.d("Developer", "Response:  $randomDogImageDto")
+                    runOnUiThread {
+                        Picasso.get().load(randomDogImageDto!!.message).into(imageView)
+                    }
+                }
             }
         }
+    }
 
-        GlobalScope.launch(Dispatchers.IO) {
-            val response = dogsImageService.getRandomImageByDogBreed("boxer")
-            if (response.isSuccessful) {
-                val randomDogImageDto = response.body()
-                Log.d("Developer", "Response:  $randomDogImageDto")
+    @OptIn(DelicateCoroutinesApi::class)
+    fun getRandomDog(dogsImageService: DogsImageService) {
+        val button = findViewById<Button>(R.id.button2)
+        button.setOnClickListener {
+            GlobalScope.launch(Dispatchers.IO) {
+                val imageView = findViewById<ImageView>(R.id.imageView)
+                val response = dogsImageService.getRandomDogImage()
+                if (response.isSuccessful) {
+                    val randomDogImageDto = response.body()
+                    Log.d("Developer", "Response:  $randomDogImageDto")
+                    runOnUiThread {
+                        Picasso.get().load(randomDogImageDto!!.message).into(imageView)
+                    }
+                }
             }
         }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
